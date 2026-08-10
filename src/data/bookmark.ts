efaa2b7b21df46f15ab56b354a0338ea0142ccba
itzfurizugg <mdzakirafifh96999@gmail.com>
2026-08-10 18:24:12 +0700

    added bookmarks library

--- src/data/bookmark.ts
+++ src/data/bookmark.ts
@@ -19,16 +19,16 @@ export const bookmarks: Bookmark[] = [
         keywords: ["git", "github"],
     },
     {
-        id: "google",
-        title: "Google",
-        url: "https://www.google.com",
-        keywords: ["google"],
+        id: "tiktok",
+        title: "TikTOk",
+        url: "https://tiktok.com",
+        keywords: ["tiktok"],
     },
     {
-        id: "reddit",
-        title: "Reddit",
-        url: "https://www.reddit.com",
-        keywords: ["reddit"],
+        id: "telegram",
+        title: "Telegram",
+        url: "https://web.telegram.org",
+        keywords: ["tele", "telegram"],
     },
     {
         id: "facebook",
@@ -42,23 +42,83 @@ export const bookmarks: Bookmark[] = [
         url: "https://web.whatsapp.com",
         keywords: ["wa", "waweb", "whatsapp"],
     },
-        {
+    {
         id: "vercel",
         title: "Vercel",
         url: "https://web.whatsapp.com",
         keywords: ["vercel", "deploy"],
     },
-        {
+    {
         id: "supabase",
         title: "Supabase",
         url: "https://web.whatsapp.com",
-        keywords: ["supabase", "sbase"],
+        keywords: ["supabase", "supa", "sbase"],
     },
-        {
+    {
         id: "figma",
         title: "Figma",
         url: "https://figma.com",
-        keywords: ["figma", "design"],
+        keywords: ["figma", "design", "fig"],
+    },
+    {
+        id: "maps",
+        title: "Google Maps",
+        url: "https://maps.google.com",
+        keywords: ["maps", "gmaps", "google maps"],
+    },
+    {
+        id: "drive",
+        title: "GOogle Drive",
+        url: "https://figma.com",
+        keywords: ["figma", "design", "fig"],
+    },
+    {
+        id: "claude",
+        title: "Claude AI",
+        url: "https://claude.ai",
+        keywords: ["claude", "best ai", "clau"],
+    },
+    {
+        id: "chatgpt",
+        title: "ChatGPT",
+        url: "https://chatgpt.com",
+        keywords: ["chatgpt", "gpt", "jews"],
+    },
+    {
+        id: "gemini",
+        title: "Gemini: Powered by Google",
+        url: "https://gemini.google.com",
+        keywords: ["gemini", "veo"],
+    },
+    {
+        id: "daisyui",
+        title: "daisyUI",
+        url: "https://daisyui.com/docs/install/",
+        keywords: ["daisy", "daisyUI"],
+    },
+    {
+        id: "sumopod",
+        title: "Sumopod: AI Slavery",
+        url: "https://sumopod.com",
+        keywords: ["sumo", "sumopod", "ai tokens", "ai token"],
+    },
+        {
+        id: "gmail",
+        title: "Google Mail",
+        url: "https://mail.google.com",
+        keywords: ["gmail", "mail", "email", "google gmail"],
+    },
+        {
+        id: "photos",
+        title: "Google Photos",
+        url: "https://photos.google.com",
+        keywords: ["photo", "photos", "foto", "google foto"],
+    },
+        {
+        id: "message",
+        title: "Google Message",
+        url: "https://messages.google.com",
+        keywords: ["gmess", "message", "rcs"],
     },
 ];
 
